0db30b5015ad581ab971692bf4d45f530a47911c
PoMa <poma@odoo.com>
2025-05-20 15:39:02 +0200

    Initial commit to run odoo from source in docker

---
+++ setup.bat
@@ -0,0 +1,3 @@
+git submodule add -b 17.0 https://github.com/odoo/odoo.git
+git submodule add -b 17.0 https://github.com/odoo/enterprise.git
+git submodule add -b 17.0-4711437-poma git@github.com:odoo-ps/psbe-sasu_seldon_fin_sas.git ./addons
